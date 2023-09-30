26aa0eac18de87091bffff92bc46eb660d3eb3a3
Tyseer <tyseer2334@gmail.com>
2023-09-30 16:07:35 -0400

    Fixed sample API Call

--- Main.java
+++ Main.java
@@ -7,10 +7,11 @@ import java.net.http.HttpResponse;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://lexicala1.p.rapidapi.com/search-entries"))
+                .uri(URI.create("https://typewise-ai.p.rapidapi.com/completion/complete"))
+                .header("content-type", "application/json")
                 .header("X-RapidAPI-Key", "e47261d986mshb7c5eb352d858d3p19fcc5jsn5c82697dba57")
-                .header("X-RapidAPI-Host", "lexicala1.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .header("X-RapidAPI-Host", "typewise-ai.p.rapidapi.com")
+                .method("POST", HttpRequest.BodyPublishers.ofString("{\r\n    \"text\": \"Please complete this sent\",\r\n    \"correctTypoInPartialWord\": false,\r\n    \"language\": \"en\"\r\n}"))
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
