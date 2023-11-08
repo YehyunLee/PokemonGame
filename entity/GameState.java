12a32d80c48bb4fb09f776cbae296d960f44ae79
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2023-11-08 18:18:17 -0500

    implemented GameState entity

--- entity/GameState.java
+++ entity/GameState.java
@@ -1,4 +1,21 @@
 package entity;
 
-public class GameState {
+public class GameState implements State {
+    private boolean turn;
+
+    GameState() {
+        this.turn = true;
+    }
+
+    @Override
+    public boolean getGameState() {
+        return turn;
+    }
+
+    public boolean changeTurn() {
+        this.turn = !turn;
+        return turn;
+    }
+
+
 }
