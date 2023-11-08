5295961aa2bd3c688e915113ff057ee175450981
Muaj Ahmed <muaj.ahmed@mail.utoronto.ca>
2023-11-08 00:38:06 -0500

    PlayerPokemons entity with method for changing chosen pokemon completed

--- src/main/java/entity/PlayerPokemons.java
+++ src/main/java/entity/PlayerPokemons.java
@@ -1,4 +1,15 @@
 package entity;
 
 public class PlayerPokemons {
+
+    Pokemon[] pokemons = new Pokemon[0];
+    int FirstPokemon = 0;
+
+    int chosenPokemon = 0;
+
+    // Create a method for changing chosenPokemon value
+    public int setChosenPokemon(int newChosenPokemon) {
+        this.chosenPokemon = newChosenPokemon;
+        return newChosenPokemon;
+    }
 }
