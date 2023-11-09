b2f709ff340a7c1491f11685af3dd2197a38d224
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2023-11-08 20:38:08 -0500

    Add GameState class to entity package, and implement class.

--- src/main/java/entity/GameState.java
+++ src/main/java/entity/GameState.java
@@ -1,4 +1,18 @@
 package entity;
 
 public class GameState {
+    private boolean turn;
+    GameState() {
+        this.turn = true;
+    }
+
+    public boolean getGameState() {
+        return turn;
+    }
+
+    public boolean changeTurn() {
+        this.turn = !turn;
+        return turn;
+    }
+
 }
