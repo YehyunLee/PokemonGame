77d9235a04bc09b9d5931bcf3d25af7fd42553b5
Tyseer <tyseer2334@gmail.com>
2023-11-09 17:58:33 -0500

    Added Dummy proofing and sperated out a lot of funtionality in DataAcess object, put classes that handle data parsing in the UseCase layer

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,6 +1,6 @@
 package app;
 
-import data_access.PokemonApiCallApiCallInterfaceObject;
+import data_access.PokemonApiCallDataAccessObject;
 import data_access.PokemonApiCallInterface;
 import data_access.PokemonListFromSpritesDataAcessObject;
 import data_access.PokemonListFromSpritesInterface;
@@ -10,12 +10,12 @@ public class Main {
     public static void main(String[] args) {
 
 
-        PokemonApiCallInterface apiDataAccess = new PokemonApiCallApiCallInterfaceObject();
+        PokemonApiCallInterface apiDataAccess = new PokemonApiCallDataAccessObject();
         PokemonListFromSpritesInterface spritesDataAccess = new PokemonListFromSpritesDataAcessObject();
 
         // Create an instance of PokemonFactoryFromData and inject the data access object
         PokemonFactoryFromData factory = new PokemonFactoryFromData(apiDataAccess, spritesDataAccess);
-        Pokemon poke = factory.createPokemonFromData("Charizard");
+        Pokemon poke = factory.createPokemonFromData("Eternatus");
         poke.printAllStats();
 
     }
