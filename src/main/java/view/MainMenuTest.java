faa956ba4e9f610e95904fae2a54775de0a43928
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2023-11-20 14:58:13 -0500

    add view dir

---
+++ src/main/java/view/MainMenuTest.java
@@ -0,0 +1,10 @@
+package view;
+import javax.swing.*;
+
+public class MainMenuTest {
+    public static void main(String[] args) {
+        MenuView menuView = new MenuView();
+        menuView.setVisible(true);
+
+    }
+}
