0151b5ef6039e6e5db75df3e56e513835e404397
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2023-11-20 16:39:31 -0500

    commit

--- src/main/java/view/MenuView.java
+++ src/main/java/view/MenuView.java
@@ -3,14 +3,11 @@ import javax.swing.*;
 public class MenuView extends JFrame {
     JFrame frame = new JFrame("Main Menu");
     JPanel panel = new JPanel();
-    JPanel panel2 = new JPanel();
-    JPanel panel3 = new JPanel();
-
 
     JButton buttonStart = new JButton("Start");
     JButton buttonExit = new JButton("Exit");
     JButton buttonMusic = new JButton("Music");
-    JLabel label = new JLabel("Welcome to the game!");
+    JLabel label = new JLabel("Welcome!");
     public MenuView() {
         panel.setLayout(null);
         label.setBounds(10, 20, 120, 25);
@@ -22,6 +19,7 @@ public class MenuView extends JFrame {
         buttonMusic.setBounds(10, 140, 80, 25);
         panel.add(buttonMusic);
         frame.add(panel);
+
         frame.setSize(300, 300);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
