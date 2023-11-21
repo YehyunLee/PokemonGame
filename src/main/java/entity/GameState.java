cda4f004d6cf9956dfc519e3601f7020dfeb73b0
Tyseer <tyseer2334@gmail.com>
2023-11-20 22:28:00 -0500

    Added Gameplay, but several bugs present, such as swithcing Pokemon and PLayer Ai not alternating proplerly need to take a deeper look, also implemented a test case with pre generated Pokemons

--- src/main/java/entity/GameState.java
+++ src/main/java/entity/GameState.java
@@ -2,14 +2,14 @@ package entity;
 
 public class GameState {
     public Boolean GameOver = false;
-    public PlayerPokemons player;
-    public PlayerPokemons aiPlayer;
+    public PlayerorAiPokemons player;
+    public PlayerorAiPokemons aiPlayer;
 
     public Pokemon ActivePlayerPokemon;
     public Pokemon ActiveAiPokemon;
 
 
-    public void initializeGameState(PlayerPokemons player, PlayerPokemons aiPlayer) {
+    public void initializeGameState(PlayerorAiPokemons player, PlayerorAiPokemons aiPlayer) {
         this.GameOver = false;
         this.player = player;
         this.aiPlayer = aiPlayer;
@@ -17,12 +17,5 @@ public class GameState {
         ActivePlayerPokemon = player.getActivePokemon();
         ActiveAiPokemon = aiPlayer.getActivePokemon();
     }
-
-
-
-
-
-
-
 
 }
